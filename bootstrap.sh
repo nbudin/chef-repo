bcf9084090d1623273a13129c036df12d1a80611
Nat Budin <natbudin@gmail.com>
2009-11-16 13:28:47 -0500

    There's no reason passenger_nginx has to require REE

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 RUBY_ENTERPRISE_URL=http://rubyforge.org/frs/download.php/66164/ruby-enterprise_1.8.7-2009.10_i386.deb
 
 apt-get -y install git-core build-essential
-curl $RUBY_ENTERPRISE_URL >/tmp/ruby-enterprise.deb
+wget -O /tmp/ruby-enterprise.deb $RUBY_ENTERPRISE_URL
 dpkg -i /tmp/ruby-enterprise.deb
 
 gem sources -a http://gems.opscode.com
@@ -19,7 +19,6 @@ echo 'file_cache_path "/tmp/chef-solo"' >/etc/chef/solo.rb
 echo 'cookbook_path "/srv/chef/cookbooks"' >>/etc/chef/solo.rb
 
 echo '{' >server-config.json
-echo ' "ree": { "architecture": "i386" },' >>server-config.json
 echo ' "recipes": ["passenger_nginx"]' >>server-config.json
 echo '}' >>server-config.json
 
