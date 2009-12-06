6f45e2d649ed32a90038e3fd5f3af2f95fd6f9ee
Nat Budin <nbudin@kenichi-2.(none)>
2009-12-05 21:41:04 -0500

    Use the right param names for a definition

--- cookbooks/rails_app/definitions/rails_app.rb
+++ cookbooks/rails_app/definitions/rails_app.rb
@@ -13,7 +13,7 @@ define :rails_app, :deploy => true do
   include_recipe "passenger_nginx"
   
   %w{config log pids sqlite system}.each do |dir|
-    directory "/srv/#{params[:app_name]}/shared/#{dir}" do
+    directory "/srv/#{params[:name]}/shared/#{dir}" do
       recursive true
       owner "www-data"
       group "www-data"
@@ -24,7 +24,7 @@ define :rails_app, :deploy => true do
   database_server = params[:db][:server] || "localhost"
   #database_server = search(:node, "database_master:true").map {|n| n['fqdn']}.first
 
-  template "/srv/#{params[:app_name]}/shared/config/database.yml" do
+  template "/srv/#{params[:name]}/shared/config/database.yml" do
     source "database.yml.erb"
     owner "www-data"
     group "www-data"
@@ -33,15 +33,15 @@ define :rails_app, :deploy => true do
   end
 
   if params[:db][:type] =~ /sqlite/
-    file "/srv/#{params[:app_name]}/shared/sqlite/production.sqlite3" do
+    file "/srv/#{params[:name]}/shared/sqlite/production.sqlite3" do
       owner "www-data"
       group "www-data"
       mode "0664"
     end
   end
 
-  deploy "/srv/#{params[:app_name]}" do
-    repo "git://github.com/radiant/radiant.git"
+  deploy "/srv/#{params[:name]}" do
+    repo params[:repo]
     branch params[:branch]
     user "www-data"
     enable_submodules false
@@ -50,15 +50,15 @@ define :rails_app, :deploy => true do
     environment params[:environment]
     shallow_clone true
     revision params[:revision]
-    action params[:action].to_sym
+    action (params[:action] || :nothing).to_sym
     restart_command "touch tmp/restart.txt"
   end
 
-  web_app "#{params[:app_name]}" do
-    docroot "/srv/#{params[:app_name]}/current/public"
-    template "#{params[:app_name]}.conf.erb"
-    server_name "#{params[:app_name]}.#{node[:domain]}"
-    server_aliases [ "#{params[:app_name]}", node[:hostname] ]
+  web_app "#{params[:name]}" do
+    docroot "/srv/#{params[:name]}/current/public"
+    template "#{params[:name]}.conf.erb"
+    server_name "#{params[:name]}.#{node[:domain]}"
+    server_aliases [ "#{params[:name]}", node[:hostname] ]
     rails_env "production"
   end
 end
