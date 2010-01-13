9f62814e72b594c879f05286e44044a269a401a6
Nat Budin <natbudin@gmail.com>
2010-01-13 16:27:46 -0500

    Make sure we have the most recent gems

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,8 @@ apt-get -y install git-core build-essential
 wget -O /tmp/ruby-enterprise.deb $RUBY_ENTERPRISE_URL
 dpkg -i /tmp/ruby-enterprise.deb
 
+gem update --system
+gem update -a
 gem sources -a http://gems.opscode.com
 gem install ohai json rake --no-ri --no-rdoc
 gem install chef --no-ri --no-rdoc
