aba51885a3b72ea78cbb6ac51727c5ee27c6b5e1
Nat Budin <natbudin@gmail.com>
2010-01-13 11:59:41 -0500

    Syntax fix

--- cookbooks/passenger_nginx/definitions/nginx_site.rb
+++ cookbooks/passenger_nginx/definitions/nginx_site.rb
@@ -9,19 +9,15 @@ define :nginx_site, :enable => true do
   end
 
   if params[:enable]
-    execute "nxensite #{params[:name]}" do
-      link "#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}" do
-        to "#{node[:nginx][:dir]}/sites-available/#{params[:name]}"
-      end
+    link "#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}" do
+      to "#{node[:nginx][:dir]}/sites-available/#{params[:name]}"
       notifies :restart, resources(:service => "nginx")
       only_if { File.exists?("#{node[:nginx][:dir]}/sites-available/#{params[:name]}") }
       not_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}") end
     end
   else
-    execute "nxdissite #{params[:name]}" do
-      link "#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}" do
-        action :delete
-      end
+    link "#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}" do
+      action :delete
       notifies :restart, resources(:service => "nginx")
       only_if do File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}") end
     end
