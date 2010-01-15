230cdf37a891d72eaaa8e5c24cd6b024422deaaf
Nat Budin <natbudin@gmail.com>
2010-01-15 11:30:54 -0500

    Add backupninja cookbook

---
+++ cookbooks/backupninja/recipes/default.rb
@@ -0,0 +1,61 @@
+package "backupninja" do
+  action :install
+end
+
+template "/etc/backupninja.conf" do
+  source "backupninja.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(
+    :report_email => node[:backupninja][:report_email]
+  )
+end
+
+template "/etc/backup.d/10.sys" do
+  source "10.sys.erb"
+  owner "root"
+  group "root"
+  mode 0600
+end
+
+if node[:backupninja][:duplicity_s3]
+  package "duplicity" do
+    action :install
+  end
+  
+  template "/etc/backup.d/90dup.sh" do
+    source "90dup.sh.erb"
+    owner "root"
+    group "root"
+    mode 0600
+    
+    includes = ["/var/spool/cron/crontabs",
+                "/var/backups",
+                "/etc",
+                "/root",
+                "/home",
+                "/usr/local",
+                "/var/lib/dpkg/status*",
+                "/opt"] +
+               (node[:backupninja][:duplicity_s3][:includes] || [])
+    excludes = ["/home/*/.gnupg",
+                "/home/*/.Trash"] +
+               (node[:backupninja][:duplicity_s3][:excludes] || []) +
+               ["**"]
+    
+    full_every = node[:backupninja][:duplicity_s3][:full_every] || "30D"
+    keep_last_full = node[:backupninja][:duplicity_s3][:keep_last_full] || 3
+    
+    variables(
+      :includes => includes,
+      :excludes => excludes,
+      :aws_access_key_id => node[:backupninja][:duplicity_s3][:aws_access_key_id],
+      :aws_secret_access_key => node[:backupninja][:duplicity_s3][:aws_secret_access_key],
+      :passphrase => node[:backupninja][:duplicity_s3][:passphrase],
+      :target_url => node[:backupninja][:duplicity_s3][:target_url],
+      :full_every => full_every,
+      :keep_last_full => keep_last_full
+    )
+  end
+end
