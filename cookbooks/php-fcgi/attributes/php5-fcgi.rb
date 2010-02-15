8001d0276c741bc46047c463ebe5c10fe5281309
Nat Budin <natbudin@gmail.com>
2010-02-15 17:28:21 -0500

    Default blank app set

--- cookbooks/php-fcgi/attributes/php5-fcgi.rb
+++ cookbooks/php-fcgi/attributes/php5-fcgi.rb
@@ -2,3 +2,7 @@ set_unless[:php][:fcgi][:user]         = "www-data"
 set_unless[:php][:fcgi][:port]         = 9000
 set_unless[:php][:fcgi][:children]     = 1
 set_unless[:php][:fcgi][:max_requests] = 100
+
+unless attribute?("php_apps")
+  php_apps {}
+end
