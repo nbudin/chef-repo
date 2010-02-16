9dfef38d71e5fa8719b0650c81daf5e2f080f66e
Nat Budin <natbudin@gmail.com>
2010-02-16 10:50:13 -0500

    Write metadata for php-fcgi

--- cookbooks/php-fcgi/metadata.rb
+++ cookbooks/php-fcgi/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "Nat Budin"
+maintainer_email "nat@sugarpond.net"
 license          "Apache 2.0"
-description      "Installs/Configures php-fcgi"
+description      "Installs/Configures PHP5 in FCGI mode for nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+depends "passenger_nginx"
