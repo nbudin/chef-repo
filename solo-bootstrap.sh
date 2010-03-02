0466a77c4e0395a4d26a278ff647f5838b8ef4fb
Nat Budin <natbudin@gmail.com>
2010-03-02 11:00:22 -0500

    Fix perms, write solo.rb

--- solo-bootstrap.sh
+++ solo-bootstrap.sh
@@ -2,21 +2,27 @@
 
 RUBY_ENTERPRISE_URL=http://rubyforge.org/frs/download.php/68720/ruby-enterprise_1.8.7-2010.01_amd64.deb
 
-apt-get -y install build-essential cron
+sudo apt-get -y install build-essential cron
 wget -O /tmp/ruby-enterprise.deb $RUBY_ENTERPRISE_URL
-dpkg -i /tmp/ruby-enterprise.deb
+sudo dpkg -i /tmp/ruby-enterprise.deb
 
-gem update --system --no-ri --no-rdoc
-gem update --no-ri --no-rdoc
-gem sources -a http://gems.opscode.com
-gem install ohai json rake --no-ri --no-rdoc
-gem install chef --no-ri --no-rdoc
+sudo gem update --system --no-ri --no-rdoc
+sudo gem update --no-ri --no-rdoc
+sudo gem sources -a http://gems.opscode.com
+sudo gem install ohai json rake --no-ri --no-rdoc
+sudo gem install chef --no-ri --no-rdoc
 
-apt-get -y install git-core
+sudo apt-get -y install git-core
 git clone http://github.com/nbudin/chef-repo.git
-cd chef-repo
-rake install
-cd ..
+
+sudo mkdir -p /etc/chef
+echo <<EOSCRIPT |sudo /bin/bash
+
+cat <<EOF >/etc/chef/solo.rb
+file_cache_path "/tmp/chef-solo"
+cookbook_path "$(pwd)/chef-repo/cookbooks"
+EOF
+EOSCRIPT
 
 cat <<EOF >solo.json
 {
