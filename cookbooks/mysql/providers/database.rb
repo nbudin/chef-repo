a432c56098cdf6a8851c465ba0e4b05a38df49c7
Nat Budin <natbudin@gmail.com>
2010-03-02 11:51:49 -0500

    Use actual SQL queries for create/drop

--- cookbooks/mysql/providers/database.rb
+++ cookbooks/mysql/providers/database.rb
@@ -3,14 +3,14 @@ include Opscode::Mysql::Database
 action :create do
   Chef::Log.info "mysql_database: creating database"
 
-  root_db.create_db(new_resource.name)
-  root_db.query("grant all on #{new_resource.name}.* to '#{new_resource.username}'@'#{node[:fqdn]}' identified by '#{new_resource.password}'")
+  root_db.query("create database if not exists `#{new_resource.name}`")
+  root_db.query("grant all on `#{new_resource.name}`.* to '#{new_resource.username}'@'#{node[:fqdn]}' identified by '#{new_resource.password}'")
 end
 
 action :drop do
   Chef::Log.info "mysql_database: dropping database"
 
-  root_db.drop_db(new_resource.name)
+  root_db.query("drop database if exists `#{new_resource.name}`")
 end
 
 action :flush_tables_with_read_lock do
