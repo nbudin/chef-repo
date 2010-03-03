2e46ff6b20b6effce011ec750b492a13f2a2b867
Nat Budin <natbudin@gmail.com>
2010-03-03 12:27:10 -0500

    Debugging

--- cookbooks/php-fcgi/definitions/php_fcgi_app.rb
+++ cookbooks/php-fcgi/definitions/php_fcgi_app.rb
@@ -2,10 +2,12 @@ define :php_fcgi_app do
   include_recipe "php-fcgi"
   include_recipe "passenger_nginx"
 
+  params[:config_template] ||= "php_fcgi_app.conf.erb"
+  params[:cookbook] ||= "php-fcgi"
+
   nginx_site params[:name] do
-    config_template "php_fcgi_app.conf.erb"
-    cookbook "php-fcgi" unless params[:cookbook]
     params.each do |k, v|
+      Chef::Log.info("Sending #{k.inspect} -> #{v.inspect} to nginx_site")
       send(k, v)
     end
   end
